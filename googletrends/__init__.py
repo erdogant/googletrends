86a9ca73efe0e413601722b71ffa4ad50f05932c
erdogant <erdogant@gmail.com>
2023-12-02 15:57:41 +0100

    0.1.2

--- googletrends/__init__.py
+++ googletrends/__init__.py
@@ -12,7 +12,7 @@ from googletrends.googletrends import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 # module level doc-string
 __doc__ = """
